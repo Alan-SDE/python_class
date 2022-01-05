bcbe2e663ac136c6b8331c0b859f41121dd1c3c2
Alan-SDE <apotsy8251@gmail.com>
2022-01-05 11:36:31 -0500

    initial commit

---
+++ challenge01-munsters.py
@@ -0,0 +1,11 @@
+
+# define a short data set (in real world, we want to read this from a file or API)
+munsters = {'endDate': 1966, 'startDate': 1964,
+        'names':['Lily', 'Herman', 'Grandpa', 'Eddie', 'Marilyn']}   # {} creates dict
+
+# Your solution goes below this line
+# ----------------------------------
+print(munsters['names'])
+print(munsters['endDate'])
+print(munsters['startDate'])
+munsters['episodes'] = 70
