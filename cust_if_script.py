44013919abf86a9384fabd4a44a97b1e76ec6e9e
Alan-SDE <apotsy8251@gmail.com>
2022-01-05 16:13:31 -0500

    added if labs

---
+++ cust_if_script.py
@@ -0,0 +1,15 @@
+def main():
+    score = int(input("What is your score (1-100)"))
+    if score > 90:
+        print("you received an A")
+    elif score > 80:
+        print("you received a B")
+    elif score > 70:
+        print("you received a C")
+    elif score > 70:
+        print("you received a D")
+    else:
+        print("you received an F")
+
+
+main()
---
+++ if-test.py
@@ -0,0 +1,6 @@
+
+hostname = input("What value should we set for hostname?")
+print hostname
+
+if hostname.lower() == "mtg":
+    print("The hostname was found to be MTG")
