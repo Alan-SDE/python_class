7e1eda271459628a1545738b6578b9afab17ce09
Alan-SDE <apotsy8251@gmail.com>
2022-01-05 12:44:24 -0500

    added string lab

---
+++ String_lab.py
@@ -0,0 +1,19 @@
+trek = "ncc 1701-d"
+a = "the prime directive"
+a = a.split(" ")
+print(a)
+a = "-".join(a)
+print (a)
+
+def main():
+
+    lilstring = "Alta3 Research offers classes on Python coding"
+    newlist = lilstring.split(" ")
+    print (newlist)
+
+    myiplist = ["192", "168", "0", "12"]
+    singleip = ".".join(myiplist)
+    print (singleip)
+
+main()
+
