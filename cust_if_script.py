71f49e5ff57238df24f50622bb30ff2643492a49
Alan-SDE <apotsy8251@gmail.com>
2022-01-05 16:25:36 -0500

    added validation

--- cust_if_script.py
+++ cust_if_script.py
@@ -4,17 +4,21 @@ def main():
     # prompt for number score
     score = int(input("What is your score (1-100)"))
 
-    # print letter grade based on number
-    if score > 90:
-        print("you received an A")
-    elif score > 80:
-        print("you received a B")
-    elif score > 70:
-        print("you received a C")
-    elif score > 70:
-        print("you received a D")
+    # validate input
+    if 1 > score or score > 100:
+        print("Invalid number, has to be between 1 and 100")
     else:
-        print("you received an F")
+        # print letter grade based on number
+        if score > 90:
+            print("you received an A")
+        elif score > 80:
+            print("you received a B")
+        elif score > 70:
+            print("you received a C")
+        elif score > 70:
+            print("you received a D")
+        else:
+            print("you received an F")
 
 
 main()
